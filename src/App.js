e7d0b4cebcded44af64cd77b61ca56e955285209
Manish <manish@gmail.com>
2022-11-10 20:53:47 +0545

    Required data setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import NormalObject from "./components/normalObject";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NormalObject />
+    </div>
+  );
 }
 
 export default App;
