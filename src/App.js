8bbad3977b7e180438408a60a6b5ebe74a02c6bc
Manish <manish@gmail.com>
2022-11-10 21:29:20 +0545

    Advanced Object is on going

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import NormalObject from "./components/normalObject";
+import AdvancedObject from "./components/advancedObject";
 
 function App() {
   return (
     <div className="App">
-      <NormalObject />
+      {/* <NormalObject></NormalObject> */}
+      <AdvancedObject></AdvancedObject>
     </div>
   );
 }
